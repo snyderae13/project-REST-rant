65c61057df00c15373bc120192a98ccdd9da8fb7
snyderae13 <snyderae13@icloud.com>
2021-12-08 19:33:25 -0600

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -46,6 +46,19 @@ router.post("/", urlencodedParser, (req, res) => {
 })
 
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id]})
+  }
+})
+
 
 
 module.exports = router

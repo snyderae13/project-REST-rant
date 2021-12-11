d0a52263e3bdcab7494472cb954b932d2e63be49
snyderae13 <snyderae13@icloud.com>
2021-12-10 18:40:17 -0600

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -55,10 +55,37 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id]})
+    res.render('places/show', { place: places[id], id })
   }
 })
 
+
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
+
+router.delete('/places/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(i, 1)
+    res.redirect('/places')
+  }
+})
 
 
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,32 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <div>
+            
+                <h1>{data.place.name}</h1>
+                <img src={data.place.pic} className="rounded float-left" alt={data.place.name}/>
+                <h2>Rating</h2>
+                <p>Not Rated</p>
+                <h2>Description</h2>
+                <p>Located in {data.place.city}, {data.place.state}, and Serving {data.place.cuisines}</p>
+            </div>
+            <div>
+                <h2>Comments</h2>
+                <p>No comments yet!</p>
+                <a href ={`/places/${data.id}/edit`} className="btn btn-warning">
+                    Edit
+                </a>
+                <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                    <button type="submit" className="btn btn-danger">
+                        Delete
+                    </button>
+
+                </form>
+            </div>
           </main>
         </Def>
     )

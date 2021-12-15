f2ef590ebb98399ba26fd933566824e9be3cb118
snyderae13 <snyderae13@icloud.com>
2021-12-14 18:04:19 -0600

    db working well

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,10 +2,10 @@ const React = require('react')
 const Def = require('../default')
 
 function index(data) {
-    let placesFormatted = data.places.map((place, index) => {
+    let placesFormatted = data.places.map((place) => {
         return (
           <div className="col-sm-6">
-            <a href={`/places/${index}`}>{place.name}</a>
+            <a href={`/places/${place.id}`}>{place.name}</a>
             <p clasName="text-center">
               {place.cuisines}
             </p>

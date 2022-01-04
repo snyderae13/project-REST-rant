c82ffc7ea3a5099dde057b3aac89435276d22d74
snyderae13 <snyderae13@icloud.com>
2022-01-04 16:28:06 -0600

    problem solved with show

--- controllers/places-control.js
+++ controllers/places-control.js
@@ -25,8 +25,10 @@ router.post('/', (req, res) => {
   })
   .catch(err => {
       console.log('err', err)
+      
       res.render('error404')
   })
+  
 })
 
 router.get('/new', (req, res) => {
@@ -34,10 +36,11 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-  db.Place.findById(req.params.id)
+  db.Place.findOne({_id: req.params.id})
   .populate('comments')
   .then(place => {
-      console.log(place.comments)
+    
+      
       res.render('places/show', { place })
   })
   .catch(err => {
